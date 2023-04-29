bec550d3b7cd5fff278650bd11333a38229ac731
kurilove <ivankurilov742@gmail.com>
2023-04-29 11:01:49 +0300

    jfajs

--- main.py
+++ main.py
@@ -1,10 +1,48 @@
-from kivy.app import App
-from kivy.uix.label import Label
+from kivymd.app import MDApp
+from kivymd.uix.button import MDFloatingActionButtonSpeedDial
+from kivymd.uix.screen import MDScreen
 
 
-class TelegramAccountApp(App):
+class Example(MDApp):
     def build(self):
-        return Label(text="hello my account ")
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "Orange"
+        return (
+            MDScreen(
+                MDFloatingActionButtonSpeedDial(
+                    id="speed_dial",
+                    hint_animation=True,
+                    root_button_anim=True,
+                )
+            )
+        )
+
+    def on_start(self):
+        data = {
+            "Python": "language-python",
+            "JS": [
+                "language-javascript",
+                "on_press", lambda x: print("pressed JS"),
+                "on_release", lambda x: print(
+                    "stack_buttons",
+                    self.root.ids.speed_dial.stack_buttons
+                )
+            ],
+            "PHP": [
+                "language-php",
+                "on_press", lambda x: print("pressed PHP"),
+                "on_release", self.callback
+            ],
+            "C++": [
+                "language-cpp",
+                "on_press", lambda x: print("pressed C++"),
+                "on_release", lambda x: self.callback()
+            ],
+        }
+        self.root.ids.speed_dial.data = data
+
+    def callback(self, *args):
+        print(args)
 
 
-TelegramAccountApp().run()
+Example().run()
